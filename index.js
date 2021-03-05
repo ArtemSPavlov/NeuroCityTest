1bd145304ff1454125169dd8ce67529b84c257a9
Artem Pavlov <pavlov329811@gmail.com>
2021-03-05 04:01:28 +0300

    add server start

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Start
+const express = require('express');
+const https = require('https');
+
+const PORT = 3000;
+const DATA_URL = 'https://jsonplaceholder.typicode.com/posts';
+const app = express();
+
+https.get(DATA_URL, (res) => {
+
+    res.on('data', (data) => {
+        console.log('Data: ', data.toString());
+    })
+
+    res.on('close', () => {
+        app.listen(PORT, () => console.log('Server start!!'));
+    })
+
+}).on('error', (e) => {
+
+    console.error(e);
+    console.log('Posts not loaded!');
+    process.exit(1);
+
+});
